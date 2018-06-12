979c27ade9096056e770f062b4f17067e03530fa
AlexisLELEU <alexis.leleu@hetic.net>
2018-06-12 13:45:47 +0200

    [FEATURE] add client detail page

--- connect_file/user_space.php
+++ connect_file/user_space.php
@@ -13,25 +13,25 @@ if (isset($_SESSION['id'])) {
     ?>
 
     <html>
-<head>
-    <title>Espace admin</title>
-    <link rel="stylesheet" href="">
-    <meta charset="UTF-8">
-</head>
-<body>
-    <main>
-    <a href="logOut.php">Se deconnecter</a><br>   
-        <input class="search" type="text" placeholder="search by name"/>
-        <div class="result-search-container">
-            <div class="result-search"></div>
-            <div class="result-search-createUser">
-                <a href="create.php">Create new user</a>
-            </div>
-        </div>     
-     </main>
-     <script src='../app.js'></script>
-</body> 
-</html>
+        <head>
+            <title>Espace admin</title>
+            <link rel="stylesheet" href="">
+            <meta charset="UTF-8">
+        </head>
+        <body>
+            <main>
+            <a href="logOut.php">Se deconnecter</a><br>   
+                <input class="search" type="text" placeholder="search by name"/>
+                <div class="result-search-container">
+                    <div class="result-search"></div>
+                    <div class="result-search-createUser">
+                        <a href="create.php">Create new user</a>
+                    </div>
+                </div>     
+            </main>
+            <script src='../app.js'></script>
+        </body> 
+    </html>
 
 <?php
 }
--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-
-
 session_start();
 
 require_once 'connect_file/connect.php';
@@ -12,40 +10,39 @@ if (isset($_POST['form_connectAdmin'])){
     $passW_connect = $_POST['passW_connect'];
 
     if (!empty($email) AND !empty($passW_connect)){
-        
-        $req_user = "SELECT `id_user`,`firstname`,`lastname`,`email`, `password` FROM user WHERE `email`= :email AND `password`=:passW_connect";
-        $co_user = $pdo->prepare($req_user);
-        $co_user->bindValue(':email', $email, PDO::PARAM_STR);
-        $co_user->bindValue(':passW_connect', $passW_connect, PDO::PARAM_STR);
-        $co_user->execute();
-        
-        $userExist = $co_user->rowCount();
 
-
-        if ($userExist == 1){
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $req_user = "SELECT `id_user`,`firstname`,`lastname`,`email`, `password` FROM user WHERE `email`= :email AND `password`=:passW_connect";
+            $co_user = $pdo->prepare($req_user);
+            $co_user->bindValue(':email', $email, PDO::PARAM_STR);
+            $co_user->bindValue(':passW_connect', $passW_connect, PDO::PARAM_STR);
+            $co_user->execute();
             
-            $userInfo = $co_user->fetch();
-            $_SESSION['email'] = $userInfo['email'];
-            $_SESSION['id'] = $userInfo['id_user'];
+            $userExist = $co_user->rowCount();
+    
+            if ($userExist == 1){
+                
+                $userInfo = $co_user->fetch();
+                $_SESSION['email'] = $userInfo['email'];
+                $_SESSION['id'] = $userInfo['id_user'];
+    
+                header('location: connect_file/user_space.php');
 
-            header('location: connect_file/user_space.php?id='.$_SESSION['id']);
-
+            } else {
+                $erreur = "L'email ou le mot de passe est incorrect.";
+            }
         } else {
-            header('location: index.php');
+            $erreur = "Veuillez entrer un email valide.";
         }
-
     } else {
-        $erreur = 'Tout les champs doivent être complétés ';
+        $erreur = 'Tout les champs doivent être complétés.';
     }
 }
-
-
 ?>
-
 
 <html>
 <head>
-    <title>Espace admin</title>
+    <title></title>
     <link rel="stylesheet" href="">
     <meta charset="UTF-8">
 </head>
@@ -54,39 +51,20 @@ if (isset($_POST['form_connectAdmin'])){
         <div class="">
             <div class="">
                 <form method="post" action="">
-                    <table>
-                        <tr>
-                            <td>
-                                <input type="text" name="email" placeholder="email">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <input type="password" name="passW_connect" placeholder="mot de passe">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <input type="submit" name="form_connectAdmin" value="se connecter">
-                            </td>
-                        </tr>
-                    </table>
+                    <input type="text" name="email" placeholder="email">
+                    <input type="password" name="passW_connect" placeholder="mot de passe">
+                    <input type="submit" name="form_connectAdmin" value="se connecter">
                 </form>
-                
             </div>
             <?php
             if (isset($erreur)){
                 ?>
                 <p class=""><?= $erreur ?></p>
-            <?php
+                <?php
             }
             ?>
         </div>
-
     </main>
-
-
     <script src='./app.js'></script>
-
 </body>
 </html>
